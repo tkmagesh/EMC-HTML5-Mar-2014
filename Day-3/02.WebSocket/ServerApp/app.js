49fb51df0b90b9b767acad0746e873b892fc0679
tkmagesh <tkmagesh77@gmail.com>
2014-03-19 12:24:03 +0530

    Chat Client

--- Day-3/02.WebSocket/ServerApp/app.js
+++ Day-3/02.WebSocket/ServerApp/app.js
@@ -2,16 +2,9 @@ var ws = require("nodejs-websocket");
 var server = ws.createServer(function(conn){
 	console.log("A new connection is established");
 	conn.on("text",function(msg){
-		if (msg === "start"){
-			var count = 0;
-			var timer = setInterval(function sendTime(){
-				conn.sendText("Current date and time is " + new Date().toString());
-				if (++count >= 10) clearInterval(timer);
-			},2000)
-		} else {
-			console.log("Invalid message " + msg);
-		}
-
+		server.connections.forEach(function(con){
+			con.sendText(msg);
+		});
 	});
 });
 server.listen(9091);
